1426095b526ae4bac0877b41e33bb4edfc83eef8
Rahul Chaudhary <rahul.ch@codestoresolutions.com>
2023-02-09 18:33:13 +0530

    Componenet Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BannerComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule
